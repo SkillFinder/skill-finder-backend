9932f9b509d04eb0246467aa03deaa4e9c0ae931
DeJw92 <dpawlak1992@gmail.com>
2016-03-23 22:09:53 +0100

    security fix

--- src/main/java/org/skillfinder/configurations/WebSecurityConfig.java
+++ src/main/java/org/skillfinder/configurations/WebSecurityConfig.java
@@ -3,12 +3,19 @@ package org.skillfinder.configurations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated().and()
+        http.authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .and()
                 .httpBasic();
     }
 }
